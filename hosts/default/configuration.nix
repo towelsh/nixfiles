1a1ffdaa5b6e614d4b1c4fc4e57c5978b734de9e
Teatowel <tea@towel.sh>
2024-03-11 18:30:14 +0000

    add catppuccin gtk theme

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -121,6 +121,7 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    gh
 
     # desktop env
     gnome.gnome-tweaks
--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -72,4 +72,18 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # configure gtk theme (todo: move to a module)
+  gtk = {
+  	enable = true;
+	theme = {
+		name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+		package = pkgs.catppuccin-gtk.override {
+			accents = [ "pink" ];
+			size = "compact";
+			tweaks = [ "rimless" "black" ];
+			variant = "macchiato";
+		};
+	};
+  };
 }
