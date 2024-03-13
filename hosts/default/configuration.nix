6622b7b954d85ed4c8af65dcc5b8d4736b3bfdec
Teatowel <tea@towel.sh>
2024-03-13 22:02:21 +0000

    add nonfree pkgs

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -138,6 +138,8 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nixpkgs.config.allowUnfree = true; # allow nonfree packages
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
